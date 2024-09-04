c74e7027f570118f19e63de4785b627439e9b405
mohamedamineberguia <mohamedamineberguia@gmail.com>
2024-09-04 00:55:04 +0000

    allgood

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const rowStyle = {
   backgroundColor: "#f5f5f5ab",
